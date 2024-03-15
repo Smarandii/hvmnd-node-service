250e5135dc6cde395920f128904fc68946339c2a
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-15 07:24:23 +0300

    node service v.0.3.2

--- update_password.ps1
+++ update_password.ps1
@@ -1,3 +1,4 @@
 # update_password.ps1
 $newPassword = Get-Content "C:\Program Files (x86)\AnyDesk\file.txt"
+& 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe' --remove-password
 & 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe' --set-password $newPassword
