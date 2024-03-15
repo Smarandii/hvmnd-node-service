cbdd9e0446f63bcbbf697a17ffddd4525dcf2c02
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-15 07:35:22 +0300

    node service v.0.3.4

--- update_password.ps1
+++ update_password.ps1
@@ -1,5 +1,4 @@
 # update_password.ps1
 $newPassword = Get-Content "C:\Program Files (x86)\AnyDesk\file.txt"
 & 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe' --remove-password _unattended_access
-& 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe' --set-password $newPassword
-echo $newPassword | 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe' --set-password _unattended_access
+& echo $newPassword | 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe' --set-password _unattended_access
