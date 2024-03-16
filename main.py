75650f9f9000f076e3f47d290631ac4d88f9334b
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-16 21:14:25 +0300

    node service v.0.5.0

--- main.py
+++ main.py
@@ -1,26 +1,79 @@
-import sys
+import os
+import socket
+import psutil
+import GPUtil
 
-from flask import Flask, request, jsonify
-import subprocess
+import ngrok
 import random
 import string
 import pathlib
+import subprocess
+
+import pymongo
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Simple authentication token for demonstration purposes
-# In production, use a more secure method for authentication
 AUTH_TOKEN = "YourSecretToken"
 PATH_TO_ANY_DESK = r"C:\Program Files (x86)\AnyDesk\AnyDesk.exe"
 PATH_TO_PW_FILE = r"C:\Program Files (x86)\AnyDesk\file.txt"
 PATH_TO_ANY_DESK = pathlib.Path(PATH_TO_ANY_DESK)
 PATH_TO_PW_FILE = pathlib.Path(PATH_TO_PW_FILE)
 TASK_NAME = "UpdateAnyDeskPassword"
+os.environ['NGROK_AUTHTOKEN'] = '2ETJzIGCEuQ8aJaGWju3nA4sswz_6xeXM7qFqhNvBXCAL8pKZ'
+MONGO_URI = "mongodb+srv://admin:WIyniFnVBpcbG1pJ@cluster0.aaaafpm.mongodb.net/?retryWrites=true&w=majority"
 
 
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for i in range(length))
+
+
+def get_system_info():
+    """
+    Returns a dictionary containing system information including CPU and GPU details.
+    """
+    # Get the machine name
+    machine_name = socket.gethostname()
+
+    # Get CPU information
+    cpu_info = psutil.cpu_freq()
+    cpu = f"{psutil.cpu_count(logical=False)} Physical, {psutil.cpu_count(logical=True)} Logical, {cpu_info.max:.2f}MHz Max Frequency"
+
+    # Attempt to get GPU information, default to None if GPUUtil is not applicable
+    gpu = "Not Found"
+    gpus = GPUtil.getGPUs()
+    if gpus:
+        gpu = ", ".join([f"{gpu.name}" for gpu in gpus])
+
+    return {"machine_name": machine_name, "cpu": cpu, "gpu": gpu}
+
+
+def save_or_update_ngrok_url(url):
+    """
+    Saves or updates the ngrok URL in MongoDB with the machine identifier.
+    """
+    client = pymongo.MongoClient(MONGO_URI)
+    db = client["new_database"]
+    collection = db["nodes"]
+
+    # Use the hostname as a simple identifier
+    machine_id = socket.gethostname()
+    system_info = get_system_info()
+
+    # Attempt to update the document if it exists, otherwise insert a new one
+    update_result = collection.update_one(
+        {"machine_id": machine_id},
+        {"$set": {"update_password_webhook": url, "cpu": system_info["cpu"], "gpu": system_info["gpu"]}},
+        upsert=True
+    )
+
+    if update_result.matched_count > 0:
+        print("Updated the ngrok URL for the existing machine ID.")
+    else:
+        print("Inserted a new document with the ngrok URL and machine ID.")
+
+    client.close()
 
 
 @app.route('/update_password', methods=['POST'])
@@ -31,7 +84,6 @@ def update_password():
 
     new_password = generate_password()
 
-    # Write the new password to a secure location
     with open(PATH_TO_PW_FILE, "w") as pwd_file:
         pwd_file.write(new_password)
 
@@ -39,7 +91,7 @@ def update_password():
         command = ["powershell.exe", "-ExecutionPolicy", "Unrestricted", "-File", "update_password.ps1"]
         process = subprocess.run(command, capture_output=True, text=True)
         if process.returncode == 0:
-            return jsonify({"new_password": new_password, "return_code": process.returncode})
+            return jsonify({"new_password": new_password})
         else:
             return jsonify({"error": "Failed to update password", "details": process.stderr}), 500
     except subprocess.CalledProcessError as e:
@@ -47,5 +99,7 @@ def update_password():
 
 
 if __name__ == '__main__':
-    # DO NOT run this app with debug=True in production!
+    listener = ngrok.forward(5000, authtoken_from_env=True)
+    print(f"Ingress established at {listener.url()}")
+    save_or_update_ngrok_url(listener.url())
     app.run(port=5000, debug=True)
