53da22fd37b468e36807bf845519e474f7403db7
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-16 14:55:42 +0300

    node service v.0.3.5

--- update_password.ps1
+++ update_password.ps1
@@ -1,4 +1,5 @@
-# update_password.ps1
-$newPassword = Get-Content "C:\Program Files (x86)\AnyDesk\file.txt"
-& 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe' --remove-password _unattended_access
-& echo $newPassword | 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe' --set-password _unattended_access
+echo $newPassword | 'C:\Program Files (x86)\AnyDesk\AnyDesk.exe' --set-password;
+>> Start-Sleep -Seconds 1;
+>> $serviceName="AnyDesk";
+>> if((Get-Service -Name $serviceName).Status -eq "Running")
+>> {Restart-Service -Name $serviceName;}
