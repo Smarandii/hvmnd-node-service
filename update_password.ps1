8789ac02d3786e65d3c9785eb18a688b3d90d653
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-16 20:33:08 +0300

    node service v.0.4.4

--- update_password.ps1
+++ update_password.ps1
@@ -3,4 +3,5 @@ echo $newPassword | anydesk --set-password;
 Start-Sleep -Seconds 1;
 $serviceName="AnyDesk";
 if((Get-Service -Name $serviceName).Status -eq "Running")
-{Restart-Service -Name $serviceName;}
+{Restart-Service -Name $serviceName;};
+echo "Success"
