dfccc7310abd5fb1375f1f75fc79a0d1630d7861
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-18 01:45:24 +0300

    node service v.1.1.4

--- update_password.ps1
+++ update_password.ps1
@@ -1,5 +1,5 @@
 $newPassword = Get-Content "C:\Program Files (x86)\AnyDesk\file.txt"
-echo $newPassword | anydesk --set-password;
+echo $newPassword | anydesk --set-password "Node User Access";
 Start-Sleep -Seconds 1;
 $serviceName="AnyDesk";
 if((Get-Service -Name $serviceName).Status -eq "Running")
