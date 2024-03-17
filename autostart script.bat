78e19c931d39a94606feaca5e2e61ee80638fb16
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-17 10:39:48 +0300

    render node v.1.1.2

--- autostart script.bat
+++ autostart script.bat
@@ -2,9 +2,9 @@
 SET projectDir=C:\Program Files (x86)\render_hive_farm_bot_node_service
 SET venvDir=%projectDir%\venv
 
-cd "%projectDir%"
+cd %projectDir%
 
-IF NOT EXIST "%venvDir%" (
+IF NOT EXIST %venvDir% (
     python -m venv venv
     echo Virtual environment created.
 )
@@ -21,3 +21,5 @@ pip install -r requirements.txt
 
 echo Starting application...
 pythonw main.py > "%projectDir%\app_output.log" 2>&1
+
+set /p UserInputPath=Press Enter to close this window...
