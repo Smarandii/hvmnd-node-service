1cb7b2dc484e8150b573c64dda6bf11e8b6dcad8
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-17 07:05:56 +0300

    node service v.1.1.0

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import ngrok
 from render_node_manager.app import app
 from render_node_manager.db_operations import save_or_update_ngrok_url
+from pyuac import main_requires_admin
 
-if __name__ == '__main__':
+
+@main_requires_admin
+def main():
     listener = ngrok.connect(5000, authtoken_from_env=True)
     print(f"Ingress established at {listener.url()}")
     save_or_update_ngrok_url(listener.url())
     app.run(port=5000, debug=True)
+
+
+if __name__ == '__main__':
+    main()
