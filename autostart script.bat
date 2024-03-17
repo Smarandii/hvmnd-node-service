36bb6bf46fc1ad30a8ec5aaa0a2326968cd424e4
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-17 08:11:20 +0300

    node service v.1.1.3

--- autostart script.bat
+++ autostart script.bat
@@ -1,10 +1,10 @@
 @echo off
-SET projectDir="C:\Program Files (x86)\render_hive_farm_bot_node_service"
-SET venvDir="%projectDir%\venv"
+SET projectDir=C:\Program Files (x86)\render_hive_farm_bot_node_service
+SET venvDir=%projectDir%\venv
 
-cd %projectDir%
+cd "%projectDir%"
 
-IF NOT EXIST %venvDir% (
+IF NOT EXIST "%venvDir%" (
     python -m venv venv
     echo Virtual environment created.
 )
@@ -21,5 +21,3 @@ pip install -r requirements.txt
 
 echo Starting application...
 pythonw main.py > "%projectDir%\app_output.log" 2>&1
-
-
