e26599308098e2f6b4b59b7e81e608b5f8a8f129
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-20 16:04:38 +0300

    add gitignore

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import ngrok
 from render_node_manager.app import app
 from render_node_manager.db_operations import save_or_update_ngrok_url
 from pyuac import main_requires_admin
+import Tkinter as tk
+import tkMessageBox as messagebox
 
 
 @main_requires_admin
@@ -15,3 +17,14 @@ if __name__ == '__main__':
     print(f"Ingress established at {listener.url()}")
     save_or_update_ngrok_url(listener.url())
     main()
+
+    root = tk.Tk()
+
+
+    def on_closing():
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            root.destroy()
+
+
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    root.mainloop()
