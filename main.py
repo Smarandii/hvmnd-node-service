336a714ab423c420d33974dd1f19a2a3cf0a08c7
Oleg Smarandi <olegsmarandi@gmail.com>
2024-03-20 16:17:13 +0300

    add gitignore

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import ngrok
 from render_node_manager.app import app
 from render_node_manager.db_operations import save_or_update_ngrok_url
 from pyuac import main_requires_admin
-import Tkinter as tk
-import tkMessageBox as messagebox
 
 
 @main_requires_admin
@@ -18,13 +16,3 @@ if __name__ == '__main__':
     save_or_update_ngrok_url(listener.url())
     main()
 
-    root = tk.Tk()
-
-
-    def on_closing():
-        if messagebox.askokcancel("Quit", "Do you want to quit?"):
-            root.destroy()
-
-
-    root.protocol("WM_DELETE_WINDOW", on_closing)
-    root.mainloop()
