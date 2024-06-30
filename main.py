ea6249f09d70f1c629fb45f6d1e92222837961b8
Олег Смаранди <o.smarandi@citydrive.ru>
2024-06-30 11:23:56 +0300

    render node service v.3.2.6

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 from pyuac import main_requires_admin
 from render_node_manager.db_operations import DBOperations
 from render_node_manager.utils import add_to_system_path
-from render_node_manager.config import PATH_TO_ANY_DESK
 
 
 @main_requires_admin
 def main():
-    add_to_system_path(PATH_TO_ANY_DESK.parent + "\\")
+    add_to_system_path("C:\\Program Files (x86)\\AnyDesk\\")
     dbo = DBOperations()
     dbo.startup_node()
     dbo.poll_node_status()
