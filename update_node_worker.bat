4a4b12b6a8cc13ac0c05c08d254f0a3af8dfb8d1
Олег Смаранди <o.smarandi@citydrive.ru>
2025-01-22 14:57:13 +0300

    render node service v6.1.6 | implement remote update mechanizm

---
+++ update_node_worker.bat
@@ -0,0 +1,44 @@
+@echo off
+setlocal
+
+:: Define log file
+set LOGFILE=update_node.log
+
+:: Start logging
+echo Updating service... > "%LOGFILE%"
+
+echo Stopping render-node-service... >> "%LOGFILE%"
+
+:: Stop the service
+sc stop render-node-service >> "%LOGFILE%" 2>&1
+timeout /t 5 >nul
+sc query render-node-service | findstr /i "STOPPED" >nul 2>&1
+if %ERRORLEVEL% NEQ 0 (
+    echo "Service did not stop successfully!" >> "%LOGFILE%"
+    exit /b 1
+)
+
+echo Service stopped successfully. >> "%LOGFILE%"
+
+:: Pull latest code
+echo Pulling latest code... >> "%LOGFILE%"
+git remote add origin-update "https://Smarandii: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@github.com/Smarandii/hvmnd-node-service.git" >> "%LOGFILE%" 2>&1
+git pull origin-update master >> "%LOGFILE%" 2>&1
+
+if %ERRORLEVEL% NEQ 0 (
+    echo Git pull failed! >> "%LOGFILE%"
+    exit /b 1
+)
+
+:: Run Python script directly
+echo Running service setup... >> "%LOGFILE%"
+.\venv\Scripts\python.exe render_node_setup.py >> "%LOGFILE%" 2>&1
+if %ERRORLEVEL% NEQ 0 (
+    echo Service setup failed! >> "%LOGFILE%"
+    exit /b 1
+)
+
+:: Finish logging
+echo Script completed successfully. >> "%LOGFILE%"
+endlocal
+exit /b 0
