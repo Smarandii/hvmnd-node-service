0f0999e4cece9aaf8b16f981ae5240c164552aa9
Олег Смаранди <o.smarandi@citydrive.ru>
2025-01-22 09:04:44 +0300

    render node service v6.0.0 | implement remote update mechanizm

--- update_node.bat
+++ update_node.bat
@@ -1,8 +1,11 @@
 @echo off
 setlocal
 
-powershell -ExecutionPolicy Bypass -Command "git pull origin master"
-powershell -ExecutionPolicy Bypass -Command ". .\venv\Scripts\activate; python render_node_setup.py"
+echo Pulling latest code...
+git pull origin master || echo Git pull failed! && exit /b 1
+
+echo Activating virtual environment and updating service...
+powershell -ExecutionPolicy Bypass -Command ". .\venv\Scripts\activate; python render_node_setup.py" || echo Service setup failed! && exit /b 1
 
 echo Script completed successfully.
 endlocal
