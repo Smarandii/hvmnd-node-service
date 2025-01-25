28b35ae6e39c41c616120d2ea08b178ba8b75745
Олег Смаранди <o.smarandi@citydrive.ru>
2025-01-25 09:26:52 +0300

    hvmnd-node-service v8.1.0 | move from heroku to personal vps

--- hvmnd_node_service_manager/config.py
+++ hvmnd_node_service_manager/config.py
@@ -3,7 +3,7 @@ import pathlib
 PATH_TO_ANY_DESK = pathlib.Path(r"C:\Program Files (x86)\AnyDesk\AnyDesk.exe")
 PATH_TO_PW_FILE = pathlib.Path(r"C:\Program Files (x86)\AnyDesk\file.txt")
 PATH_TO_LOG_FILE = pathlib.Path(r"C:\Program Files (x86)\AnyDesk\logs.log")
-PG_URL = "postgresql://u392hn6janvdjd:p63b381c7fe01cb1f1e89cf0549dd4a6fb8be076c9af3d03b71e29692ee94624d@c7vbm80blivm58.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d40tce2ulgaggj"
+PG_URL = "postgres://postgres:8IXAog3W7XDjX0k62WLbPFUrJc5owQqPjojp1Vnhm3oXbO4w1AnxzC3I0K9rOIE8@q8swwoww0g0k8c8gkwgocsso:5432/postgres?sslmode=disable"
 ALERT_BOT_TOKEN = "6524183208:AAHXOGhNtuQ1mHis-3J9_tsd01ZI0CTIX60"
 ADMIN_CHAT_ID = "231584958"
 UPDATE_PW_POWERSHELL_COMMAND = f"""
