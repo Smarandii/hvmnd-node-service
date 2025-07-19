4dda019f162063adff4077061de3e3e79d4ad6dc
Smarandii <olegsmarandi@gmail.com>
2025-07-19 13:51:12 +0300

    change api service addr

--- hvmnd_node_service_manager/config.py
+++ hvmnd_node_service_manager/config.py
@@ -3,7 +3,7 @@ import pathlib
 PATH_TO_ANY_DESK = pathlib.Path(r"C:\Program Files (x86)\AnyDesk\AnyDesk.exe")
 PATH_TO_PW_FILE = pathlib.Path(r"C:\Program Files (x86)\AnyDesk\file.txt")
 PATH_TO_LOG_FILE = pathlib.Path(r"C:\Program Files (x86)\AnyDesk\logs.log")
-HVMND_API_CLIENT_BASE_URL = "http://prod.hvmnd-api.freemyip.com/api/v1"
+HVMND_API_CLIENT_BASE_URL = "http://95.217.142.240:9876/api/v1"
 RESTARTING_DISABLED = False
 HVMND_API_TOKEN = "67509949-e9b4-800f-a556-eb61cbff2d81"
 ALERT_BOT_TOKEN = "7309039400:AAGIvZDu4w6Zq4afAGOtkHlmf9tmfFvSYek"
